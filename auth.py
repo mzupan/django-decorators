bd798a129308a8e0d1d94f62441fdee1fd040516
Mike Zupan <mzupan@laptop.shopopensky.com>
2010-03-29 11:57:57 -0400

    adding files

---
+++ auth.py
@@ -0,0 +1,22 @@
+from functools import wraps
+
+from django.contrib.auth.models import Group
+from django.http import Http404
+
+def group_required(groups=[]):
+    def decorator(func):
+        def inner_decorator(request,*args, **kwargs):
+            
+            
+            for group in groups:
+                try:
+                    if Group.objects.get(name=group) in request.user.groups.all():
+                        return func(request, *args, **kwargs)
+                except:
+                    pass
+            
+            raise Http404()
+        
+        return wraps(func)(inner_decorator)
+    
+    return decorator
